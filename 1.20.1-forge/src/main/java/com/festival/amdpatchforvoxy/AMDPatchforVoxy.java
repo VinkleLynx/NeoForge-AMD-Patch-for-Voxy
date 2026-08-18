a1b029b980dfc579b62b3596a762e6606d35f265
Festival <connor.kingwill@gmail.com>
2026-08-18 09:57:29 +0800

    mv to 1.20.1 branch

--- 1.20.1-forge/src/main/java/com/festival/amdpatchforvoxy/AMDPatchforVoxy.java
+++
@@ -1,9 +0,0 @@
-package com.festival.amdpatchforvoxy;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(AMDPatchforVoxy.MODID)
-public class AMDPatchforVoxy
-{
-    public static final String MODID = "amdpatchforvoxy";
-}
