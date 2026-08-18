5019778e00abc17330fc42af79be9a917f013062
Festival <connor.kingwill@gmail.com>
2026-08-18 09:55:48 +0800

    mv to 1.20.1 branch

--- src/main/java/com/festival/amdpatchforvoxy/AMDPatchforVoxy.java
+++ src/main/java/com/festival/amdpatchforvoxy/AMDPatchforVoxy.java
@@ -1,9 +1,9 @@
 package com.festival.amdpatchforvoxy;
 
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(AMDPatchforVoxy.MODID)
-public class AMDPatchforVoxy {
+public class AMDPatchforVoxy
+{
     public static final String MODID = "amdpatchforvoxy";
 }
